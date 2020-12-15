3447f44291b15b8eac67684276b58790549e0063
ashamabee <65267655+ashamabee@users.noreply.github.com>
2020-12-14 20:13:05 -0800

    SQL

--- Data_to_SQL/imdb.sql
+++ Data_to_SQL/imdb.sql
@@ -10,11 +10,36 @@ CREATE TABLE imdb_movies (
 	avg_vote FLOAT,
 	budget INT,
 	gross_income_usa INT,
-	gross_income_global INT,
+	gross_income_global FLOAT,
 	votes INT,
-	user_rating FLOAT,
+	user_reviews FLOAT,
 	critics_reviews FLOAT,
 	profit FLOAT
 );
 
 SELECT * FROM imdb_movies;
+
+
+
+
+
+
+-- title_id             object
+-- title                object
+-- year                  int64
+-- genre                object
+-- duration              int64
+-- director             object
+-- actors               object
+-- description          object
+-- avg_vote            float64
+-- budget                int64
+-- gross_income_usa      int64
+-- gross_income_global      int64
+-- user_reviews         float64
+-- critics_reviews     float64
+-- genres               object
+
+
+
+
